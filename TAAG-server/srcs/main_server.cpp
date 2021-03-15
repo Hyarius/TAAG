e6252f26051bb3ac27432d98a0a00e48d924ecc6
Debian <debian@packer-output-41cc5919-c984-4646-8a05-c17f6f0f87bc>
2021-03-15 21:31:49 +0000

    Adding makefile

--- TAAG-server/srcs/main_server.cpp
+++ TAAG-server/srcs/main_server.cpp
@@ -6,13 +6,15 @@ void save_server(int sig)
 {
 	serv->save();
 	serv->quit();
+	exit(0);
 }
 
 int main(int argc, char** argv)
 {
 	serv = new Server();
 	signal(SIGINT, save_server);
-	signal(SIGBREAK, save_server);
+	signal(SIGQUIT, save_server);
+	signal(SIGTSTP, save_server);
 	serv->run();
 	serv->save();
 	return (0);
